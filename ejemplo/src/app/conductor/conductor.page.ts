e890f02bc2ec0d21cc078aef0c1dcff5a20a4c08
jtvalenzuela <jt.valenzuela@duocuc.cl>
2024-09-06 20:47:11 -0400

    vista pasajero lista!

--- ejemplo/src/app/conductor/conductor.page.ts
+++ ejemplo/src/app/conductor/conductor.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-conductor',
   templateUrl: './conductor.page.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConductorPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
+
+  MandarAlogin(){
+    this.navCtrl.navigateForward('/login');
+  }
 
   ngOnInit() {
   }
