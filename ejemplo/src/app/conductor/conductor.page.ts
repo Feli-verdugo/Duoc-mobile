43b4e8b8af2771fd84beae49fbddbf6d83eda0d5
feli-verdugo <feli.verdugo@duocuc.cl>
2024-09-11 13:22:02 -0300

    Pagina del conductor yera

--- ejemplo/src/app/conductor/conductor.page.ts
+++ ejemplo/src/app/conductor/conductor.page.ts
@@ -9,6 +9,15 @@ export class ConductorPage implements OnInit {
 
   constructor(private navCtrl: NavController) { }
 
+  clientes = [
+    { nombre: 'Cliente 1', costo: '$2000' },
+    { nombre: 'Cliente 2', costo: '$1800' },
+    { nombre: 'Cliente 3', costo: '$1900' },
+    { nombre: 'Cliente 4', costo: '$2300' },
+    { nombre: 'Cliente 5', costo: '$3000' },
+    { nombre: 'Cliente 6', costo: '$2500' },
+  ];
+
   MandarAlogin(){
     this.navCtrl.navigateForward('/login');
   }
